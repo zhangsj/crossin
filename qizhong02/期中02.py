a3743e55b18a8f9d47e31ca387529329afda36e8
zhangsj <289696142@qq.com>
2017-06-08 14:52:55 +0800

    猜数字修复乱码，自动创建game.txt

--- qizhong02/期中02.py
+++ qizhong02/期中02.py
@@ -15,17 +15,17 @@ while True:
             game_times = int(score[0])
             min_times = int(score[1])
             total_times = int(score[2])
-            print '欢迎\t'
-            print '欢迎', name, '首次来玩！'
+            # print u'欢迎\t'
+            print '\n欢迎%s首次来玩！' % name
         else:
             game_times = int(score[0])
             min_times = int(score[1])
             total_times = int(score[2])
             avg_times = float(total_times) / game_times
-            print '欢迎%s祝你游戏愉快！\n你已经玩了%d次，最少%d轮猜出答案，平均%.2f轮猜出答案' % (name, game_times, min_times, avg_times)
+            print '\n欢迎%s祝你游戏愉快\n你已经玩了%d次,最少%d轮猜出答案，平均%.2f轮猜出答案' % (name, game_times, min_times, avg_times)
     num = randint(1, 100)
     times = 1
-    print '你猜是几',num
+    print '\n你猜是几',num
     bingo = False
     while bingo == False:
         while True:
@@ -42,7 +42,7 @@ while True:
                     if answer > num:
                         print '%d 太大了!\n' % answer
                     if answer == num:
-                        print '猜中了！！答案就是%d \n' % answer
+                        print '\n猜中了！！答案就是%d' % answer
                         bingo = True
                         break
                     times += 1
@@ -58,8 +58,8 @@ while True:
     if  times < min_times:
         min_times = times
     scores[name] = [str(game_times), str(min_times), str(total_times)]
-    print '恭喜你猜中了答案就是',num
-    print '你一共猜了 %d 次\n你一共完了%d次游戏\n你平均%.2f次猜中答案\n你最好的成绩是%d次！\n' % (total_times, game_times, avg_times, min_times)
+    # print '恭 喜 你 猜 中 了 答 案 就 是',num
+    print '\n你一共猜了%d次\n你一共完了%d次游戏\n你平均%.2f次猜中答案\n你最好的成绩是%d次！\n' % (total_times, game_times, avg_times, min_times)
     result = ''
     for n in scores:
         line = n + ' ' + ' '.join(scores[n]) + '\n'
@@ -68,7 +68,7 @@ while True:
         outf.write(result)
     g=raw_input('输入“go”再玩一次，否则退出游戏   ')
     if g=='go':
-        print '新游戏'
+        print '\n新游戏'
         continue
     else:
         break
