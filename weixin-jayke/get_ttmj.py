8fc558cef62d9b117ce2ec1c20157aa8dcea4ee8
zhangsj <289696142@qq.com>
2017-08-16 15:29:38 +0800

    Update get_ttmj.py

--- weixin-jayke/get_ttmj.py
+++ weixin-jayke/get_ttmj.py
@@ -7,15 +7,29 @@ from bs4 import BeautifulSoup
 # dynm=raw_input('输入名字：')
 
 def get_mj(mjmz):
+    auth_url = 'http://www.ttmeiju.com/index.php/user/login.html'
+    data = {
+        'password': 'a226385906',
+        'username': 'jayke'
+    }
+    headers = {
+        "Host": "www.ttmeiju.com",
+        "Referer": "http://www.ttmeiju.com"
+    }
+    post_data = urllib.urlencode(data)
+    cookieJar = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
+    opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')]
     url = 'http://www.ttmeiju.com/index.php/search/index.html?keyword=%s&range=1' % mjmz
     try:
-        r = requests.get(url)
-        info_list = BeautifulSoup(r.text, "lxml").find_all('tr', class_=['Scontent', 'Scontent1'])
+        op = opener.open(url)
+        r=op.read()
+        info_list = BeautifulSoup(r, "lxml").find_all('tr', class_=['Scontent', 'Scontent1'])
         if len(info_list)==0:#没搜索到返回没找到
             mjinfo="没找到。。。。。。"
         else:
             mjinfo = ''
-            for i in info_list[:4]:#由于字数限制只取前四个
+            for i in info_list[:3]:#由于字数限制只取前3个
                 info=i.find_all('td')
                 name1=info[1].find('a').string
                 link=''
